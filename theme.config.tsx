b84d024daae762d63fb8893b470c03afe82bb89d
Tucker Triggs <tuckertriggs@pm.me>
2023-06-20 06:23:58 +0000

    remove discord chat link

--- theme.config.tsx
+++ theme.config.tsx
@@ -6,9 +6,6 @@ const config: DocsThemeConfig = {
   project: {
     link: "https://github.com/tuckpuck/adastack",
   },
-  chat: {
-    link: "https://discord.com",
-  },
   docsRepositoryBase: "https://github.com/tuckpuck/adastack/blob/main/",
   footer: {
     text: "adastack",
