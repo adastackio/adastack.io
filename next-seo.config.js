9f4a127cc2ed101a678d9f1fd357c0c0f4c0d8c2
tuckpuck <tucker@tuckertriggs.com>
2023-06-22 12:57:29 +0700

    attempt to add SEO config

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  titleTemplate: "Adastack.io | %s",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://www.url.ie/",
+    siteName: "SiteName",
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
