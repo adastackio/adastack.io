1de35fdd5978a8b8d4d8bc65dd11813174770074
tuckpuck <tucker@tuckertriggs.com>
2023-06-23 20:35:31 +0700

    small formatting changes

--- components/tableofcontents.tsx
+++ components/tableofcontents.tsx
@@ -1,10 +1,9 @@
-import Link from 'next/link'
-import Head from 'next/head'
 import type { NextraThemeLayoutProps } from 'nextra'
  
 export default function TableOfContents({ pageOpts }: NextraThemeLayoutProps) { 
   const { title } = pageOpts 
-  console.log(title);
+  console.log("Title:", title);
+  console.log("pageOpts:", pageOpts);
   
   return (
     <div>
