0ca377c809e1b2735bb36b691fcab23eeed7a397
tuckpuck <tucker@tuckertriggs.com>
2023-07-10 11:24:44 +0600

    add card components to markdown file, and svgr package to turn svg into react components

---
+++ components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as WarningIcon } from './warning.svg'
+export { default as CardsIcon } from './cards.svg'
+export { default as OneIcon } from './one.svg'
--- next.config.js
+++ next.config.js
@@ -3,4 +3,19 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  webpack(config) {
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/;
+
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test(".svg")
+    );
+    fileLoaderRule.exclude = allowedSvgRegex;
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
