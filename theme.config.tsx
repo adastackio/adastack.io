b9a9c210db8da4f64aa119dd561824330c3d513b
tuckpuck <tucker@tuckertriggs.com>
2023-07-20 15:18:13 +0600

    change feedback link to 'suggest a tool', remove toc from home page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,36 +1,15 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
-
 const config: DocsThemeConfig = {
   useNextSeoProps() {
-		return {
-			titleTemplate: '%s | Adastack.io',
-		};
-	},
-  themeSwitch: {
-    useOptions() {
-      return {
-        light: 'Light',
-        dark: 'Dark',
-        system: 'System'
-      }
-    }
+    return {
+      titleTemplate: "%s | Adastack.io",
+    };
   },
-  // banner: {
-  //   key: '2.0-release',
-  //   text: (
-  //     <a href="https://nextra.site" target="_blank">
-  //       🎉 Nextra 2.0 is released. Read more →
-  //     </a>
-  //   )
-  // },
-  search: {placeholder: "Search Tools"},
   logo: (
     <>
-      <span style={{ fontWeight: 800 }}>
-        Adastack 
-      </span>
+      <span style={{ fontWeight: 800 }}>Adastack</span>
       &nbsp;
       <svg width="24" height="24" viewBox="0 0 24 24">
         <path
@@ -40,6 +19,7 @@ const config: DocsThemeConfig = {
       </svg>
     </>
   ),
+  search: { placeholder: "Search Tools" },
   project: {
     // link: "https://github.com/tuckpuck/adastack",
   },
@@ -52,6 +32,24 @@ const config: DocsThemeConfig = {
   //         d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07a50.338 50.338 0 0 0 22.8-.87C27.8 117.2 10.85 96.5 10.85 72.46v-.64a50.18 50.18 0 0 0 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71a143.333 143.333 0 0 0 104.08 52.76 50.532 50.532 0 0 1 14.61-48.25c20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26a50.69 50.69 0 0 1-22.2 27.93c10.01-1.18 19.79-3.86 29-7.95a102.594 102.594 0 0 1-25.2 26.16z"
   //       />
   //     </svg>
+  //   )
+  // },
+  themeSwitch: {
+    useOptions() {
+      return {
+        light: "Light",
+        dark: "Dark",
+        system: "System",
+      };
+    },
+  },
+  gitTimestamp: null,
+  // banner: {
+  //   key: '2.0-release',
+  //   text: (
+  //     <a href="https://nextra.site" target="_blank">
+  //       🎉 Nextra 2.0 is released. Read more →
+  //     </a>
   //   )
   // },
   sidebar: {
@@ -68,17 +66,24 @@ const config: DocsThemeConfig = {
     // }
   },
   toc: {
-    float: true
+    float: true,
   },
   footer: {
     text: (
       <>
-      <a href="https://adastackbeta.vercel.app/suggestatool">Suggest a Tool</a>
+        <a href="https://adastackbeta.vercel.app/suggestatool">
+          Suggest a Tool
+        </a>
       </>
-    )
+    ),
   },
-  docsRepositoryBase: "https://github.com/tuckpuck/adastack/blob/main/"
+  feedback: {
+    content: "Suggest a Tool",
+    useLink() {
+      return "suggestatool"
+    }
+  },
+  docsRepositoryBase: "https://github.com/tuckpuck/adastack/blob/main/",
 };
-
 
 export default config;
