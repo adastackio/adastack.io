6e9dcc94093ceac4699ae82dde690c25f8bf2e9f
tuckpuck <tucker@tuckertriggs.com>
2023-07-24 09:13:24 +0600

    add demo of pill icons under tools

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as WarningIcon } from './warning.svg'
 export { default as CardsIcon } from './cards.svg'
 export { default as OneIcon } from './one.svg'
+export { default as OpenSource } from './opensource.svg'
