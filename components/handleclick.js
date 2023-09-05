84f892862beeadf0fe87947a1b90fde79554f281
tuckpuck <tucker@tuckertriggs.com>
2023-09-05 11:01:28 +0500

    update mobile menu to close other open dropdowns, and to keep selected one open on reclick

--- components/handleclick.js
+++ components/handleclick.js
@@ -9,7 +9,19 @@ function handleClick(event) {
   sidebarElements.forEach((sidebarElement) => {
     if (mediaSmall.matches) {
       hamburgerMenu.click();
-      if (homeCardTextContent === sidebarElement.textContent) {
+      if (
+        // Click the menu dropdown if it has the same text as the home button
+        // and if it is not open already
+        homeCardTextContent === sidebarElement.textContent &&
+        !sidebarElement.parentNode.parentNode.classList.contains("open")
+      ) {
+        sidebarElement.click();
+      } else if (
+        // Close menu dropdowns that don't have same text as the home button
+        // and are open already
+        homeCardTextContent !== sidebarElement.textContent &&
+        sidebarElement.parentNode.parentNode.classList.contains("open")
+      ) {
         sidebarElement.click();
       }
     }
