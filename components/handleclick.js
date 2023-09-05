bbd363f9b6bf2a92a14027827b14226d0669c8f6
tuckpuck <tucker@tuckertriggs.com>
2023-09-05 10:23:22 +0500

    add media query so handleClick opens hamburger menu on mobile

--- components/handleclick.js
+++ components/handleclick.js
@@ -1,9 +1,23 @@
 function handleClick(event) {
   const homeCardTextContent = event.target.textContent;
   const sidebarElements = document.querySelectorAll(".sidebar-menu-item");
+  const hamburgerMenu = document.querySelector(".nextra-hamburger");
+  const mediaSmall = window.matchMedia("(max-width: 767px)");
+  const mediaLarge = window.matchMedia("(min-width: 768px)");
+  // Check if the media query is true
+
   sidebarElements.forEach((sidebarElement) => {
-    if (homeCardTextContent === sidebarElement.textContent) {
-      sidebarElement.click();
+    if (mediaSmall.matches) {
+      hamburgerMenu.click();
+      if (homeCardTextContent === sidebarElement.textContent) {
+        sidebarElement.click();
+      }
+    }
+
+    if (mediaLarge.matches) {
+      if (homeCardTextContent === sidebarElement.textContent) {
+        sidebarElement.click();
+      }
     }
   });
 }
