3276be642b4a9387ae41078d0fbe5d7b06598bf1
tuckpuck <tucker@tuckertriggs.com>
2024-01-03 09:33:10 +0100

    attempt to access title via frontmatter

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,18 +12,8 @@ const nunito = Nunito({
 const config: DocsThemeConfig = {
   useNextSeoProps() {
     const { frontMatter } = useConfig();
-    const pageTitle = "%s";
-    console.log(pageTitle);
+    console.log(frontMatter)
 
-    const wordToRemove = "cardano ";
-    const lowerCaseWordToRemove = wordToRemove.toLowerCase();
-    const lowerCasePageTitle = pageTitle.toLowerCase();
-    let titleWithoutCardano;
-    if (lowerCasePageTitle.startsWith(lowerCaseWordToRemove)) {
-      let titleWithoutCardano = lowerCasePageTitle.replace(lowerCaseWordToRemove, "");
-    }
-    console.log("Modified string:", titleWithoutCardano);
-    
     return {
       titleTemplate: "%s | Adastack",
       description: frontMatter.seo_description ? frontMatter.seo_description : "Adastack is an open-source index of tools and resources on Cardano. Explore the ecosystem, staking, Dapps, NFTs, Catalyst, governance, and dev tools.",
