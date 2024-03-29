226c8e585a9c1cf624ca1cbc11d9927677b11724
tuckpuck <tucker@tuckertriggs.com>
2024-03-29 07:41:34 +0100

    refactor menu to click hamburger menu only once per click

--- components/handleclick.js
+++ components/handleclick.js
@@ -7,14 +7,16 @@ function handleClick(event) {
 
   // Function to open the hamburger menu and open a sub-menu in the navigation when clicking a home button
 
+  if (mediaSmall.matches) {
+    console.log("Page is small. Click hamburger menu");
+    // open hamburger menu
+    hamburgerMenu.click();
+  }
+
   sidebarElements.forEach((sidebarElement) => {
     if (mediaSmall.matches) {
-      console.log("Page is small. Click hamburger menu");
-      // open hamburger menu
-      hamburgerMenu.click();
-
       if (
-        // Initiate dropdown if dropdown nav item has the same text as the home button and is not open already
+        // Initiate dropdown if sidebar item has the same text as card and is not open already
         cardTextContent === sidebarElement.textContent &&
         !sidebarElement.parentNode.parentNode.classList.contains("open")
       ) {
@@ -23,8 +25,7 @@ function handleClick(event) {
         );
         sidebarElement.click();
       } else if (
-        // Close dropdowns that don't have same text as the home button
-        // and are open already
+        // Close dropdowns where sidebar item text does not match card and they are open already
         cardTextContent !== sidebarElement.textContent &&
         sidebarElement.parentNode.parentNode.classList.contains("open")
       ) {
