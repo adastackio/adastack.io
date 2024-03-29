c9a645f11c8a7a9bfe29bb0c040abdda9067411d
tuckpuck <tucker@tuckertriggs.com>
2024-03-29 06:54:32 +0100

    add a condition to check if menu is open

--- components/handleclick.js
+++ components/handleclick.js
@@ -1,39 +1,48 @@
 function handleClick(event) {
-  const homeCardTextContent = event.target.textContent;
   const sidebarElements = document.querySelectorAll(".sidebar-menu-item");
   const hamburgerMenu = document.querySelector(".nextra-hamburger");
+  const cardTextContent = event.target.textContent;
   const mediaSmall = window.matchMedia("(max-width: 767px)");
   const mediaLarge = window.matchMedia("(min-width: 767px)");
   // Check if the media query is true
 
   sidebarElements.forEach((sidebarElement) => {
-    if (mediaSmall.matches) {
+    if (
+      mediaSmall.matches &&
+      !sidebarElement.parentNode.parentNode.classList.contains("open")
+    ) {
       hamburgerMenu.click();
+      console.log("media matches");
       if (
-        // Click the menu dropdown if it has the same text as the home button
+        // Initiate dropdown menu if dropdown has the same text as the home button
         // and if it is not open already
-        homeCardTextContent === sidebarElement.textContent
+        cardTextContent === sidebarElement.textContent &&
+        !sidebarElement.parentNode.parentNode.classList.contains("open")
       ) {
+        console.log("condition1");
         sidebarElement.click();
       } else if (
         // Close menu dropdowns that don't have same text as the home button
         // and are open already
-        homeCardTextContent !== sidebarElement.textContent &&
+        cardTextContent !== sidebarElement.textContent &&
         sidebarElement.parentNode.parentNode.classList.contains("open")
       ) {
+        console.log("condition2");
         sidebarElement.click();
       }
     }
 
     if (mediaLarge.matches) {
-      if (homeCardTextContent === sidebarElement.textContent) {
+      if (cardTextContent === sidebarElement.textContent) {
+        console.log("condition3");
         sidebarElement.click();
       } else if (
         // Close menu dropdowns that don't have same text as the home button
         // and are open already
-        homeCardTextContent !== sidebarElement.textContent &&
+        cardTextContent !== sidebarElement.textContent &&
         sidebarElement.parentNode.parentNode.classList.contains("open")
       ) {
+        console.log("condition4");
         sidebarElement.click();
       }
     }
