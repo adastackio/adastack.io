9d546ef283836971d02e9eba116961faa26ae64f
tuckpuck <tucker@tuckertriggs.com>
2024-04-02 08:07:35 +0200

    add another condition to prevent initial mobile double click

--- components/handlecardclick.js
+++ components/handlecardclick.js
@@ -1,5 +1,6 @@
 function handleCardClick(event) {
   const hamburgerMenu = document.querySelector(".nextra-hamburger");
+  const hamburgerInner = document.querySelector(".nextra-hamburger > svg");
   const sidebarElements = document.querySelectorAll(".sidebar-menu-item");
   const cardTextContent = event.target.textContent;
   const mediaSmall = window.matchMedia("(max-width: 767px)");
@@ -7,8 +8,8 @@ function handleCardClick(event) {
 
   // Open the hamburger on mobile when clicking a Card
   const openHamburger = () => {
-    if (mediaSmall.matches) {
-      console.log("Page is small. Click hamburger menu");
+    if (mediaSmall.matches && hamburgerInner.classList.contains("open")) {
+      console.log("Page is small. Menu is closed. Click hamburger menu");
       // open hamburger menu
       hamburgerMenu.click();
     }
