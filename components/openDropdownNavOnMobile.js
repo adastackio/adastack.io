d2838dce5919b7922784378209bd307515aa91fc
tuckpuck <tucker@tuckertriggs.com>
2024-04-04 08:34:19 +0200

    add console log for hitting the route

--- components/openDropdownNavOnMobile.js
+++ components/openDropdownNavOnMobile.js
@@ -3,6 +3,8 @@ const hamburgerInner = document.querySelector(".nextra-hamburger > svg");
 const mediaSmall = window.matchMedia("(max-width: 767px)");
 const mediaLarge = window.matchMedia("(min-width: 767px)");
 
+console.log("hit open dropdown script");
+
 if (mediaSmall.matches && !hamburgerInner.classList.contains("open")) {
   console.log("on mobile. open automatically.");
   hamburgerMenu.click();
