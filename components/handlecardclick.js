c9be213388b2a74449241dc89431607d76293b71
tuckpuck <tucker@tuckertriggs.com>
2024-04-04 07:18:18 +0200

    remove initate sidebar fn content to see if it fixes issue

--- components/handlecardclick.js
+++ components/handlecardclick.js
@@ -15,58 +15,7 @@ function handleCardClick(event) {
     }
   };
 
-  const InitiateSidebarActions = () => {
-    sidebarElements.forEach((sidebarElement) => {
-      if (mediaSmall.matches) {
-        if (
-          // Open sidebar item with the same text as Card and is not open already
-          cardTextContent === sidebarElement.textContent &&
-          !sidebarElement.parentNode.parentNode.classList.contains("open")
-        ) {
-          console.log(
-            `Page is small. Text matches and it is not open already, open sidebar ${cardTextContent}`
-          );
-          sidebarElement.click();
-        } else if (
-          // Close sidebar items where text doesn't match Card and they are open
-          cardTextContent !== sidebarElement.textContent &&
-          sidebarElement.parentNode.parentNode.classList.contains("open")
-        ) {
-          console.log(
-            `Page is small. Text matches and is open already, close sidebar ${
-              cardTextContent !== sidebarElement.textContent
-                ? sidebarElement.textContent
-                : ""
-            }`
-          );
-          sidebarElement.click();
-        }
-      }
-
-      if (mediaLarge.matches) {
-        if (cardTextContent === sidebarElement.textContent) {
-          // Open sidebar item with the same text as Card
-          console.log(
-            `Page is large. Text matches, open sidebar ${cardTextContent}`
-          );
-          sidebarElement.click();
-        } else if (
-          // Close sidebar items where text doesn't match Card and they are open
-          cardTextContent !== sidebarElement.textContent &&
-          sidebarElement.parentNode.parentNode.classList.contains("open")
-        ) {
-          console.log(
-            `Page is large. Text matches and is open already, close sidebar  ${
-              cardTextContent !== sidebarElement.textContent
-                ? sidebarElement.textContent
-                : ""
-            }`
-          );
-          sidebarElement.click();
-        }
-      }
-    });
-  };
+  const InitiateSidebarActions = () => {};
 
   setTimeout(openHamburger(), 20000);
   InitiateSidebarActions();
