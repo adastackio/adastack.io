cb94c1f474f0e2b29af82d7e71f697702e0c223f
tuckpuck <tucker@tuckertriggs.com>
2024-04-04 08:15:48 +0200

    attempt removing fn shell to see if it runs

--- components/openDropdownNavOnMobile.js
+++ components/openDropdownNavOnMobile.js
@@ -1,15 +1,9 @@
-openDropdownNavOnMobile = () => {
-  const hamburgerMenu = document.querySelector(".nextra-hamburger");
-  const hamburgerInner = document.querySelector(".nextra-hamburger > svg");
-  const mediaSmall = window.matchMedia("(max-width: 767px)");
-  const mediaLarge = window.matchMedia("(min-width: 767px)");
+const hamburgerMenu = document.querySelector(".nextra-hamburger");
+const hamburgerInner = document.querySelector(".nextra-hamburger > svg");
+const mediaSmall = window.matchMedia("(max-width: 767px)");
+const mediaLarge = window.matchMedia("(min-width: 767px)");
 
-  if (mediaSmall.matches && !hamburgerInner.classList.contains("open")) {
-    console.log("on mobile. open automatically.");
-    hamburgerMenu.click();
-  }
-};
-
-openDropdownNavOnMobile();
-
-export { openDropdownNavOnMobile };
+if (mediaSmall.matches && !hamburgerInner.classList.contains("open")) {
+  console.log("on mobile. open automatically.");
+  hamburgerMenu.click();
+}
