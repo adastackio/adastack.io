ac1d0181b1699d3dcd34cbc215aa0f249d234b77
tuckpuck <tucker@tuckertriggs.com>
2024-05-04 12:53:06 -0600

    uninstall plausible package and add plausible analytics via script

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import PlausibleProvider from "next-plausible";
+import { Plausible } from "../scripts/plausible.js";
 import "../css/styles.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain="adastack.io">
+    <Plausible>
       <Component {...pageProps} />
-    </PlausibleProvider>
+    </Plausible>
   );
 }
