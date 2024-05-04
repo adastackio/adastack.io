4563f0f358bba7ee4022fecd196ddf11d6a06bc1
tuckpuck <tucker@tuckertriggs.com>
2024-05-04 12:34:22 -0600

    add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+import PlausibleProvider from "next-plausible";
 import "../css/styles.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <PlausibleProvider domain="adastack.io">
       <Component {...pageProps} />
-    </>
+    </PlausibleProvider>
   );
 }
