e7777c2b19a016b85d28f054bfef8d134449edac
tuckpuck <tucker@tuckertriggs.com>
2024-05-16 10:11:06 -0600

    fix console errors

--- components/cubeanimation.js
+++ components/cubeanimation.js
@@ -490,10 +490,10 @@ resx, resy : number of pixels of the screen
       <div className="animation-title-container">
         <h2 className="animation-title">Cardano Library</h2>
         <p className="animation-subtitle">100+ pages of resources</p>
-        <button class="h-10 mx-2 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline hover:bg-indigo-900 hover:text-indigo-100">
+        <button className="h-10 mx-2 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline hover:bg-indigo-900 hover:text-indigo-100">
           Intro Guide
         </button>
-        <button class="h-10 mx-1 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline hover:bg-indigo-900 hover:text-indigo-100">
+        <button className="h-10 mx-1 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline hover:bg-indigo-900 hover:text-indigo-100">
           Explore All
         </button>
       </div>
