a5c1b6a34b1a5bf9571f1c4546b77373318fe2ca
tuckpuck <tuckertriggs@pm.me>
2024-09-13 15:03:03 -0600

    Remove "explore all" button event listeners and add a link to "all pages" instead

--- components/hero.js
+++ components/hero.js
@@ -40,20 +40,20 @@ function Hero() {
           <li>Please vote for us in Catalyst Fund 13! 🙏</li>
         </ul>
         <div className="animation-button-container">
-          <button
-            className="animation-button mx-1 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline"
-            title="Explore All"
-            onClick={() => handleHeroButtonClick(event)}
-          >
-            Explore All
-          </button>
-          <button
-            className="animation-button-mobile mx-1 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline"
-            title="Explore All"
-            onClick={() => handleHeroButtonClickMobile(event)}
-          >
-            Explore All
-          </button>
+          <a href="/all_pages">
+            <button
+              className="animation-button mx-1 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline"
+              title="Explore All"
+            >
+              Explore All
+            </button>
+            <button
+              className="animation-button-mobile mx-1 px-5 mt-4 text-white transition-colors duration-150 border border-white rounded-lg focus:shadow-outline"
+              title="Explore All"
+            >
+              Explore All
+            </button>
+          </a>
         </div>
       </div>
     </div>
