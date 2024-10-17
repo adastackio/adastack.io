78c249cfa4421b1e9dbad940a270ea610df73ae1
tuckpuck <tuckertriggs@pm.me>
2024-10-16 22:52:52 -0600

    refactor dev teams list into a table

--- components/StarBadge.js
+++ components/StarBadge.js
@@ -2,13 +2,14 @@ const StarBadge = ({ githubURL }) => {
   const userPath = new URL(githubURL).pathname.slice(1).replace(/\/$/, ""); // Extract organization/user path and remove trailing slash
   return (
     <>
-      &nbsp;&nbsp;⟡&nbsp;&nbsp;
+      &nbsp;&nbsp; &nbsp;&nbsp;
       <a href={githubURL} className="shields_io_inline-badge">
         <img
           alt="GitHub link and star count"
           src={`https://img.shields.io/github/stars/${userPath}`}
         />
       </a>
+      &nbsp;&nbsp; &nbsp;&nbsp;
     </>
   );
 };
