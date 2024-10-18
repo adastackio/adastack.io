0d96560dc7e2eb10dce8b9ea9c8325456d9f3fb9
tuckpuck <tuckertriggs@pm.me>
2024-10-17 22:02:03 -0600

    create custom Favicon component

---
+++ components/Favicon.tsx
@@ -0,0 +1,19 @@
+type FaviconProps = {
+  url: string | null;
+};
+
+const Favicon = ({ url }: FaviconProps) => {
+  return (
+    <div className="inline-flex size-6 items-center justify-center shrink-0 rounded-md border bg-background p-1 favicon-custom-css">
+      <img
+        loading="lazy"
+        width="64"
+        height="64"
+        className="aspect-square size-6 rounded"
+        src={`https://www.google.com/s2/favicons?sz=128&domain_url=${url}`}
+      />
+    </div>
+  );
+};
+
+export default Favicon;
