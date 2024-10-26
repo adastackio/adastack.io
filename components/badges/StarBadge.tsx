abbbfac7e4ab160246225a603d66bfe8e26a8c79
tuckpuck <tuckertriggs@pm.me>
2024-10-25 20:08:09 -0600

    refactor badges and add new svgs

---
+++ components/badges/StarBadge.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StarIcon } from "@components/icons";
+
+interface StarBadgeProps {
+  teamURL: string;
+  stars: number | null;
+  error: string | null;
+}
+
+const StarBadge: React.FC<StarBadgeProps> = React.memo(
+  ({ teamURL, stars, error }) => {
+    if (!teamURL) {
+      return "";
+    }
+
+    const getBadgeContent = () => {
+      if (error) {
+        console.log(error);
+      }
+      if (stars !== null) {
+        return stars.toLocaleString();
+      } else {
+        return "...";
+      }
+    };
+
+    return (
+      <a href={teamURL} target="_blank" rel="noopener noreferrer">
+        <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-sm text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+          <StarIcon />
+          &nbsp;
+          <span className="stars">{getBadgeContent()}</span>
+        </span>
+      </a>
+    );
+  }
+);
+
+export default StarBadge;
