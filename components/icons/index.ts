88b4c668777bf5f4c53473bddf169cf96a2d3b06
tuckpuck <tuckertriggs@pm.me>
2024-10-26 00:22:19 -0600

    add x link to nav

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,7 +1,7 @@
 export { default as AdaStackDark } from "./adastacklogo_dark.svg";
 export { default as AdaStackMid } from "./adastacklogo_mid.svg";
 export { default as AdaStackLight } from "./adastacklogo_light.svg";
-export { default as XLogo } from "./x.svg";
+export { default as XIcon } from "./x.svg";
 export { default as GithubIcon } from "./github.svg";
 export { default as OpenSourceIcon } from "./opensource.svg";
 export { default as StarIcon } from "./star.svg";
