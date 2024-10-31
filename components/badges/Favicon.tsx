d1104fbc2eeffe861b24eeec6e8ff987d311a0a3
tuckpuck <tuckertriggs@pm.me>
2024-10-31 14:42:12 -0600

    add favicons to table

--- components/badges/Favicon.tsx
+++ components/badges/Favicon.tsx
@@ -4,12 +4,10 @@ type FaviconProps = {
 
 const Favicon = ({ url }: FaviconProps) => {
   return (
-    <div className="inline-flex size-6 items-center justify-center shrink-0 rounded-md border bg-background p-1 favicon-custom-css">
+    <div className="inline-flex size-6 items-center justify-center shrink-0 rounded-md bg-background p-1 favicon-custom-css">
       <img
         loading="lazy"
-        width="64"
-        height="64"
-        className="aspect-square size-6 rounded"
+        className="aspect-square h-3"
         src={`https://www.google.com/s2/favicons?sz=128&domain_url=${url}`}
       />
     </div>
