63400c382ccc6cde46642d6897f6f2883bb26f95
tuckpuck <tuckertriggs@pm.me>
2024-11-01 22:04:41 -0600

    rename vars and add a column for total github repos

--- components/tables/DataTable.tsx
+++ components/tables/DataTable.tsx
@@ -2,24 +2,30 @@ import StarBadge from "../badges/StarBadge";
 import LatestCommitBadge from "../badges/LatestCommitBadge";
 import Favicon from "../badges/Favicon";
 
-interface TeamMember {
+interface CardanoProject {
   name: string;
   website: string;
   teamURL: string;
   type: string;
-  stars?: number;
-  error?: string | null;
-  mostRecentRepo?: {
-    timeSinceLastCommit: string;
+  stars: number;
+  repos: any[];
+  mostRecentRepo: {
     url: string;
+    timeSinceLastCommit: string;
+    name: string;
+    description: string;
+    stars: number;
+    language: string | null;
+    pushedAt: string;
   };
+  error: string | null;
 }
 
 interface DataTableProps {
-  teamMembers: TeamMember[];
+  cardanoProjects: CardanoProject[];
 }
 
-export default function DataTable({ teamMembers }: DataTableProps) {
+export default function DataTable({ cardanoProjects }: DataTableProps) {
   return (
     <div className="mt-8">
       <table className="nx-block nx-overflow-x-scroll nextra-scrollbar nx-mt-6 nx-p-0 first:nx-mt-0">
@@ -32,6 +38,9 @@ export default function DataTable({ teamMembers }: DataTableProps) {
               Sum of GitHub Stars
             </th>
             <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">
+              Total Repos
+            </th>
+            <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">
               Most Recent Commit
             </th>
             <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">
@@ -40,42 +49,45 @@ export default function DataTable({ teamMembers }: DataTableProps) {
           </tr>
         </thead>
         <tbody>
-          {teamMembers.map((member, index) => (
+          {cardanoProjects.map((project, index) => (
             <tr
               className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20"
               key={index}
             >
               <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 nx-whitespace-nowrap">
                 <a
-                  href={member.website}
+                  href={project.website}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
                 >
-                  <Favicon url={member.website} />
-                  {member.name}
+                  <Favicon url={project.website} />
+                  {project.name}
                 </a>
               </td>
               <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">
                 <StarBadge
-                  teamURL={member.teamURL}
-                  stars={member.stars || null}
-                  error={member.error || null}
+                  teamURL={project.teamURL}
+                  stars={project.stars}
+                  error={project.error}
                 />
               </td>
+              <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 text-sm">
+                {project.repos ? project.repos.length : "N/A"}
+              </td>
               <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">
-                {member.mostRecentRepo ? (
+                {project.mostRecentRepo && (
                   <>
                     <span className="text-xs text-gray-500">
-                      {member.mostRecentRepo.timeSinceLastCommit}
+                      {project.mostRecentRepo.timeSinceLastCommit}
                     </span>
                     <br />
-                    <LatestCommitBadge repoURL={member.mostRecentRepo.url} />
+                    <LatestCommitBadge repoURL={project.mostRecentRepo.url} />
                   </>
-                ) : null}
+                )}
               </td>
               <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 whitespace-nowrap">
-                {member.type}
+                {project.type}
               </td>
             </tr>
           ))}
