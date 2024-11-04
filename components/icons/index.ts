ba7c38196e7f4b2ee7616894c77cb0ea14cd1bfe
tuckpuck <tuckertriggs@pm.me>
2024-11-03 23:15:33 -0700

    fix github nav component

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -3,5 +3,6 @@ export { default as AdaStackMid } from "./adastacklogo_mid.svg";
 export { default as AdaStackLight } from "./adastacklogo_light.svg";
 export { default as XIcon } from "./x.svg";
 export { default as GithubIcon } from "./github.svg";
+export { default as GithubNavIcon } from "./github_nav.svg";
 export { default as OpenSourceIcon } from "./opensource.svg";
 export { default as StarIcon } from "./star.svg";
