ac9bc9257f521c76ea00edfc1222b7385b11cf36
tuckpuck <tuckertriggs@pm.me>
2024-11-04 08:32:25 -0700

    remove ring from badges on dark mode

--- components/badges/LatestCommitBadge.tsx
+++ components/badges/LatestCommitBadge.tsx
@@ -9,7 +9,7 @@ const LatestCommitBadge = ({ repoURL }) => {
   return (
     <a href={repoURL} target="_blank" rel="noopener noreferrer">
       <span
-        className="last-commit-badge-content inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-sm text-blue-700 ring-1 ring-inset ring-blue-700/10 nx-whitespace-nowrap custom-badge
+        className="last-commit-badge-content inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-sm text-blue-700 ring-1 ring-inset dark:ring-0 ring-blue-700/10 nx-whitespace-nowrap custom-badge
                        dark:bg-blue-900/30 dark:text-blue-300 dark:ring-blue-400/30 text-blue-700 dark:text-blue-300 dark:fill-blue-300 dark:fill-opacity-75"
       >
         <OpenSourceIcon />
