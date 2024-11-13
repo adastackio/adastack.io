9386037d4ae9abf6adfac48291c4db2a3e72ee59
tuckpuck <tuckertriggs@pm.me>
2024-11-13 18:32:26 +0100

    add ConfigProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../css/styles.css";
+import { ConfigProvider } from "antd";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <ConfigProvider direction="rtl">
+        <Component {...pageProps} />
+      </ConfigProvider>
     </>
   );
 }
