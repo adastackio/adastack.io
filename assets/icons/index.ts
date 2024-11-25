d7333e82ac9d18dbc31636ef99f04b7a2ce14845
tuckpuck <tuckertriggs@pm.me>
2024-11-25 21:41:01 +0100

    add oss icon

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,10 +1,8 @@
-export { default as AdaStackDark } from "./adastacklogo_dark.svg";
-export { default as AdaStackMid } from "./adastacklogo_mid.svg";
-export { default as AdaStackLight } from "./adastacklogo_light.svg";
 export { default as XIcon } from "./x.svg";
 export { default as GithubIcon } from "./github.svg";
 export { default as GithubNavIcon } from "./github_nav.svg";
 export { default as OpenSourceIcon } from "./opensource.svg";
+export { default as OSIcon } from "./oss.svg";
 export { default as StarIcon } from "./star.svg";
 export { default as CopyIcon } from "./copy.svg";
 export { default as CopySuccessIcon } from "./copy_success.svg";
