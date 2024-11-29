f7ae11b48979e293db0088c9972cacc3df9d5fe6
tuckpuck <tuckertriggs@pm.me>
2024-11-29 10:38:30 +0100

    load campton font and add to relevant items with CSS

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,40 @@ import "../css/styles.css";
 import { StyleProvider } from "@ant-design/cssinjs";
 import { ConfigProvider, theme } from "antd";
 import { useEffect, useState } from "react";
+import localFont from "next/font/local";
+
+const campton = localFont({
+  src: [
+    { path: "../assets/fonts/0_campton_thin.otf", weight: "100" },
+    { path: "../assets/fonts/1_campton_extra_light.otf", weight: "200" },
+    { path: "../assets/fonts/2_campton_light.otf", weight: "300" },
+    { path: "../assets/fonts/3_campton_normal.otf", weight: "400" },
+    { path: "../assets/fonts/4_campton_medium.otf", weight: "500" },
+    { path: "../assets/fonts/5_campton_semi_bold.otf", weight: "600" },
+    { path: "../assets/fonts/6_campton_bold.otf", weight: "700" },
+    { path: "../assets/fonts/7_campton_extra_bold.otf", weight: "800" },
+    { path: "../assets/fonts/8_campton_black.otf", weight: "900" },
+  ],
+  variable: "--font-campton",
+  fallback: [
+    "Segoe UI",
+    "Roboto",
+    "Noto Sans",
+    "Ubuntu",
+    "Cantarell",
+    "Helvetica Neue",
+    "ui-sans-serif",
+    "system-ui",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Arial",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+    "Noto Color Emoji",
+  ],
+});
 
 const ThemeWrapper = ({ children }) => {
   const [isDark, setIsDark] = useState(false);
@@ -42,7 +76,7 @@ const ThemeWrapper = ({ children }) => {
           },
         }}
       >
-        {children}
+        <div className={campton.variable}>{children}</div>
       </ConfigProvider>
     </StyleProvider>
   );
