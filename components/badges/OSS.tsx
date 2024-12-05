9aca481f6fc1b73ac48dfd96435399aca4af4f54
tuckpuck <tuckertriggs@pm.me>
2024-12-05 16:28:00 +0100

    create OSS component badge and add OSS and descriptions to Stake Monitoring Tools

---
+++ components/badges/OSS.tsx
@@ -0,0 +1,15 @@
+import { OSIcon } from '../../assets/icons'
+
+const OSS = ({ repoURL, className }) => {
+  if (!repoURL) return null;
+
+  return (
+    <span className={`inline-block h-3 w-3 ${className}`}>
+      <a href={repoURL} target="_blank" rel="noopener noreferrer">
+        <OSIcon />
+      </a>
+    </span>
+  );
+}
+
+export default OSS;
