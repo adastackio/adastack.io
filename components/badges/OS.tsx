48a1b025af17cad330421ac9d45046489fd7adb5
tuckpuck <tuckertriggs@pm.me>
2024-12-08 14:12:01 +0100

    add OS padding

--- components/badges/OS.tsx
+++ components/badges/OS.tsx
@@ -3,8 +3,12 @@ import { OSIcon } from "../../assets/icons";
 const OS = ({ repoURL, className }) => {
   if (!repoURL) return null;
 
+  const spanClass = className
+    ? `open-source-icon-inline inline-block h-3 w-3 ${className}`
+    : "open-source-icon-inline inline-block h-3 w-3";
+
   return (
-    <span className={`inline-block h-3 w-3 ${className}`}>
+    <span className={spanClass}>
       <a href={repoURL} target="_blank" rel="noopener noreferrer">
         <OSIcon />
       </a>
