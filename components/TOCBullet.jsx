1c495469fc1aa7bab763aecc921b2f493cf5a709
tuckpuck <tuckertriggs@pm.me>
2024-12-18 00:19:37 +0100

    improve "all pages" TOC items

--- components/TOCBullet.jsx
+++ components/TOCBullet.jsx
@@ -5,10 +5,10 @@ import { BlueBullet } from "../assets/icons";
 const TOCBullet = ({ pageName, relativeUrl }) => {
   return (
     <div className="toc-item group relative">
-      <BlueBullet className="h-2 inline-block mb-[3.7px]" />
+      <BlueBullet className="h-2 w-2 inline-block mb-[3.7px] bullet" />
       <Link
-        className="toc-link pt-2 pb-2 px-4 ml-3 inline-block dark:!text-indigo-100 !text-blue-950 opacity-80 dark:opacity-100 text-lg transition-transform duration-300"
         href={relativeUrl}
+        className="toc-link pt-2 pb-2 px-1 ml-3 inline-block dark:!text-indigo-100 !text-blue-950 opacity-80 dark:opacity-100 text-lg transition-transform duration-300"
       >
         {pageName}
       </Link>
