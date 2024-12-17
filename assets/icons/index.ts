d9f9f166eb64714daad2de7127f94d8f254e3791
tuckpuck <tuckertriggs@pm.me>
2024-12-17 23:30:41 +0100

    add blue bullets to All Pages TOC

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -8,4 +8,5 @@ export { default as CopyIcon } from "./copy.svg";
 export { default as CopySuccessIcon } from "./copy_success.svg";
 export { default as SingleCommitIcon } from "./single_commit.svg";
 export { default as CardanoIcon } from "./cardano.svg";
+export { default as BlueBullet } from "./bluebullet.svg";
 export { default as BlankFallback } from "./blank_fallback.svg";
--- components/TOCBullet.jsx
+++ components/TOCBullet.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import { BlueBullet } from "../assets/icons";
 
 const TOCBullet = ({ pageName, relativeUrl }) => {
   return (
     <div className="toc-item group relative">
+      <BlueBullet className="h-2 inline-block mb-[3.7px]" />
       <Link
         className="toc-link pt-2 pb-2 px-4 ml-3 inline-block dark:!text-indigo-100 !text-blue-950 opacity-80 dark:opacity-100 text-lg transition-transform duration-300"
         href={relativeUrl}
