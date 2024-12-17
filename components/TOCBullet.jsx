246fc02fb3db24a772e060bff3cb128f667180de
tuckpuck <tuckertriggs@pm.me>
2024-12-18 00:25:42 +0100

    TOC adjustments

--- components/TOCBullet.jsx
+++ components/TOCBullet.jsx
@@ -8,7 +8,7 @@ const TOCBullet = ({ pageName, relativeUrl }) => {
       <BlueBullet className="h-2 w-2 inline-block mb-[3.7px] bullet" />
       <Link
         href={relativeUrl}
-        className="toc-link pt-2 pb-2 px-1 ml-3 inline-block dark:!text-indigo-100 !text-blue-950 opacity-80 dark:opacity-100 text-lg transition-transform duration-300"
+        className="toc-link pt-2 pb-2 px-2 ml-3 inline-block dark:!text-indigo-100 !text-blue-950 opacity-80 dark:opacity-100 text-lg transition-transform duration-300"
       >
         {pageName}
       </Link>
