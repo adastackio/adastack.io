0c2d84c4ac851731b6ac97ed023f2f39840672fd
tuckpuck <tuckertriggs@pm.me>
2024-12-21 10:53:50 +0100

    add redirects to all_pages for all top level categories

--- next.config.js
+++ next.config.js
@@ -15,6 +15,78 @@ module.exports = withNextra({
   },
   async redirects() {
     return [
+      // Redirects to All Pages, leave these as permanent: false
+      {
+        source: "/intro_to_cardano",
+        destination: "/all_pages#intro-to-cardano",
+        permanent: false,
+      },
+      {
+        source: "/staking:path(|/)",
+        destination: "/all_pages#staking",
+        permanent: false,
+      },
+      {
+        source: "/community",
+        destination: "/all_pages#community",
+        permanent: false,
+      },
+      {
+        source: "/ecosystem",
+        destination: "/all_pages#ecosystem",
+        permanent: false,
+      },
+      {
+        source: "/dapps",
+        destination: "/all_pages#dapps",
+        permanent: false,
+      },
+      {
+        source: "/nfts",
+        destination: "/all_pages#nfts",
+        permanent: false,
+      },
+      {
+        source: "/gaming",
+        destination: "/all_pages#gaming",
+        permanent: false,
+      },
+      {
+        source: "/governance",
+        destination: "/all_pages#governance",
+        permanent: false,
+      },
+      {
+        source: "/catalyst",
+        destination: "/all_pages#project-catalyst",
+        permanent: false,
+      },
+      {
+        source: "/daos",
+        destination: "/all_pages#daos",
+        permanent: false,
+      },
+      {
+        source: "/development",
+        destination: "/all_pages#development",
+        permanent: false,
+      },
+      {
+        source: "/on_chain_metrics",
+        destination: "/all_pages#on-chain-metrics",
+        permanent: false,
+      },
+      {
+        source: "/partner_chains",
+        destination: "/all_pages#partner-chains",
+        permanent: false,
+      },
+      {
+        source: "/layer_2s",
+        destination: "/all_pages#layer-2s",
+        permanent: false,
+      },
+      // Permanent Redirects
       {
         source: "/live_pages", // old URL
         destination: "/all_pages", // new URL
