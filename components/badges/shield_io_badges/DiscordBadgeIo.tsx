0c1151f736c48324dba42954ea9ad41230f615a2
tuckpuck <tuckertriggs@pm.me>
2024-12-22 08:10:54 +0100

    replace dashes with "en-dashes" so dashes can be passed for Channel Name without breaking formatting

--- components/badges/shield_io_badges/DiscordBadgeIo.tsx
+++ components/badges/shield_io_badges/DiscordBadgeIo.tsx
@@ -13,6 +13,9 @@ const DiscordBadgeIo: React.FC<DiscordBadgeIoProps> = ({
     return null;
   }
 
+  // Replace dashes with HTML entity for en dash
+  const formattedChannelName = discordChannelName.replace(/-/g, "–");
+
   return (
     <a
       href={discordInviteURL}
@@ -24,7 +27,7 @@ const DiscordBadgeIo: React.FC<DiscordBadgeIoProps> = ({
         className="shield_io_badge shields_io_repo_badge"
         alt={`${discordChannelName} Discord`}
         src={`https://img.shields.io/badge/${encodeURIComponent(
-          discordChannelName
+          formattedChannelName
         )}-ffffff?style=flat&logo=discord&logoColor=7289DA&labelColor=dfe8f0`}
       />
     </a>
