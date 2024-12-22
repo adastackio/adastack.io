9300c41dde0c310777bdebb31ce21752a1d5e161
tuckpuck <tuckertriggs@pm.me>
2024-12-22 07:25:44 +0100

    add DiscordBadge to LibraryInfoBar

--- components/LibraryInfoBar.tsx
+++ components/LibraryInfoBar.tsx
@@ -2,17 +2,29 @@ import React from "react";
 import RepoShieldIo from "./badges/shield_io_badges/RepoShieldIo";
 import LanguageShieldIo from "./badges/shield_io_badges/LanguageShieldIo";
 import LatestCommitBadgeIo from "./badges/shield_io_badges/LatestCommitBadgeIo";
+import DiscordBadgeIo from "./badges/shield_io_badges/DiscordBadgeIo";
 
 interface LibraryInfoBarProps {
   repoURL: string;
   language: string;
+  discordInviteURL: string;
+  discordChannelName: string;
 }
 
-const LibraryInfoBar = ({ repoURL, language }: LibraryInfoBarProps) => {
+const LibraryInfoBar = ({
+  repoURL,
+  language,
+  discordInviteURL,
+  discordChannelName,
+}: LibraryInfoBarProps) => {
   return (
     <div className="code-library-info-bar flex gap-2 items-center">
       <LanguageShieldIo language={language} isColorChanging={false} />
       <LatestCommitBadgeIo repoURL={repoURL} />
+      <DiscordBadgeIo
+        discordInviteURL={discordInviteURL}
+        discordChannelName={discordChannelName}
+      />
       <RepoShieldIo repoURL={repoURL} />
     </div>
   );
