9ccf6bc2353d203f55aec0da90aad74049c5b87f
tuckpuck <tuckertriggs@pm.me>
2024-12-22 17:23:57 +0100

    import Docs component into library, and make all items conditional on their data

--- components/LibraryInfoBar.tsx
+++ components/LibraryInfoBar.tsx
@@ -3,6 +3,7 @@ import RepoShieldIo from "./badges/shield_io_badges/RepoShieldIo";
 import LanguageShieldIo from "./badges/shield_io_badges/LanguageShieldIo";
 import LatestCommitBadgeIo from "./badges/shield_io_badges/LatestCommitBadgeIo";
 import DiscordBadgeIo from "./badges/shield_io_badges/DiscordBadgeIo";
+import DocsShieldIo from "./badges/shield_io_badges/DocsShieldIo";
 
 interface DiscordInfo {
   discordInviteURL: string;
@@ -12,6 +13,7 @@ interface DiscordInfo {
 interface LibraryInfoBarProps {
   repoURL: string;
   language: string;
+  docsURL: string;
   discord: DiscordInfo[];
 }
 
@@ -19,11 +21,15 @@ const LibraryInfoBar: React.FC<LibraryInfoBarProps> = ({
   repoURL,
   language,
   discord,
+  docsURL,
 }) => {
   return (
     <div className="code-library-info-bar flex gap-2 items-center">
-      <LanguageShieldIo language={language} isColorChanging={false} />
-      <LatestCommitBadgeIo repoURL={repoURL} />
+      {language && (
+        <LanguageShieldIo language={language} isColorChanging={false} />
+      )}
+      {docsURL && <DocsShieldIo docsURL={docsURL} />}
+      {repoURL && <LatestCommitBadgeIo repoURL={repoURL} />}
       {discord &&
         discord.length > 0 &&
         discord.map((info, index) => (
@@ -33,7 +39,7 @@ const LibraryInfoBar: React.FC<LibraryInfoBarProps> = ({
             discordChannelName={info.discordChannelName}
           />
         ))}
-      <RepoShieldIo repoURL={repoURL} />
+      {repoURL && <RepoShieldIo repoURL={repoURL} />}
     </div>
   );
 };
