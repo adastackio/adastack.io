14b12b28ebd0f7f03c3d3819252f63caa00e982b
tuckpuck <tuckertriggs@pm.me>
2024-12-22 07:24:53 +0100

    standardize links in badges

--- components/badges/shield_io_badges/LatestCommitBadgeIo.tsx
+++ components/badges/shield_io_badges/LatestCommitBadgeIo.tsx
@@ -12,18 +12,21 @@ const LatestCommitBadgeIo = ({ repoURL }: LatestCommitBadgeIoProps) => {
 
   const isGitLab = url.hostname === "gitlab.com";
 
-
   const latestURL = isGitLab
     ? `${url.origin}/${owner}/${repo}/-/commits/`
     : `${url.origin}/${owner}/${repo}/commits/`;
-
 
   const shieldUrl = isGitLab
     ? `https://img.shields.io/gitlab/last-commit/${owner}/${repo}?color=white&labelColor=dfe8f0`
     : `https://img.shields.io/github/last-commit/${owner}/${repo}?color=white&labelColor=dfe8f0`;
 
   return (
-    <a className="inline-block"  target="_blank" rel="noopener noreferrer" href={latestURL}>
+    <a
+      className="inline-block"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={latestURL}
+    >
       <img
         src={shieldUrl}
         className="shield_io_badge badge-io-custom-shading shields_io_repo_badge"
