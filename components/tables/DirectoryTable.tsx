c2f557ec78f88f241fc09f2a2e499abf03612813
tuckpuck <tuckertriggs@pm.me>
2025-01-06 08:12:19 +0100

    improve tables responsivity

--- components/tables/DirectoryTable.tsx
+++ components/tables/DirectoryTable.tsx
@@ -16,51 +16,53 @@ interface DirectoryTableProps {
 const DirectoryTable: React.FC<DirectoryTableProps> = ({ projects = [] }) => {
   // Add default empty array
   return (
-    <table className="data-table directory-table">
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {projects?.map(
-          (
-            project,
-            index // Add optional chaining
-          ) => (
-            <tr
-              key={index}
-              className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20"
-            >
-              <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell first-column-cell">
-                <div className="flex items-center justify-between w-full">
-                  <span className="flex items-center">
-                    <Favicon url={project.website} />
-                    &nbsp;
-                    <a href={project.website}>
-                      <span className="team_table_name_container">
-                        {project.name}
-                      </span>
-                    </a>
-                  </span>
-                </div>
-              </td>
-              <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
-                {project.description}
-              </td>
-              <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
-                <TeamGithubBadge
-                  teamGithubURL={project.teamGithubURL}
-                  text="Team"
-                />
-              </td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </table>
+    <div className="data-table-wrapper">
+      <table className="data-table directory-table">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects?.map(
+            (
+              project,
+              index // Add optional chaining
+            ) => (
+              <tr
+                key={index}
+                className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20"
+              >
+                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell first-column-cell">
+                  <div className="flex items-center justify-between w-full">
+                    <span className="flex items-center">
+                      <Favicon url={project.website} />
+                      &nbsp;
+                      <a href={project.website}>
+                        <span className="team_table_name_container">
+                          {project.name}
+                        </span>
+                      </a>
+                    </span>
+                  </div>
+                </td>
+                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
+                  {project.description}
+                </td>
+                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
+                  <TeamGithubBadge
+                    teamGithubURL={project.teamGithubURL}
+                    text="Team"
+                  />
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- components/tables/WalletsTable.tsx
+++ components/tables/WalletsTable.tsx
@@ -31,49 +31,51 @@ const WalletsTable: React.FC<WalletsTableProps> = ({ wallets, filterBy }) => {
   });
 
   return (
-    <table className="data-table">
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedWallets.map((wallet, index) => (
-          <tr
-            key={index}
-            className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20"
-          >
-            <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell first-column-cell">
-              <div className="flex items-center justify-between w-full">
-                <span className="flex items-center">
-                  <Favicon url={wallet.website} />
-                  &nbsp;
-                  <a href={wallet.website}>
-                    <span className="team_table_name_container">
-                      {wallet.name}
-                    </span>
-                  </a>
-                </span>
-              </div>
-            </td>
-            <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
-              <TeamGithubBadge
-                teamGithubURL={wallet.teamGithubURL}
-                text="Team"
-              />
-            </td>
-            <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
-              <GithubRepoBadge
-                repoURL={wallet.walletRepoURL}
-                text="Wallet Code"
-              />
-            </td>
+    <div className="data-table-wrapper">
+      <table className="data-table wallet-table">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {sortedWallets.map((wallet, index) => (
+            <tr
+              key={index}
+              className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20"
+            >
+              <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell first-column-cell">
+                <div className="flex items-center justify-between w-full">
+                  <span className="flex items-center">
+                    <Favicon url={wallet.website} />
+                    &nbsp;
+                    <a href={wallet.website}>
+                      <span className="team_table_name_container">
+                        {wallet.name}
+                      </span>
+                    </a>
+                  </span>
+                </div>
+              </td>
+              <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
+                <TeamGithubBadge
+                  teamGithubURL={wallet.teamGithubURL}
+                  text="Team"
+                />
+              </td>
+              <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
+                <GithubRepoBadge
+                  repoURL={wallet.walletRepoURL}
+                  text="Wallet Code"
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
