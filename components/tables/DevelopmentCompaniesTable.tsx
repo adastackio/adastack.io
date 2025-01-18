a98bd8d22e02bff8daa5c2c725135e5b482d42a5
tuckpuck <tuckertriggs@pm.me>
2025-01-18 07:23:21 -0700

    filter by dev teams

--- components/tables/DevelopmentCompaniesTable.tsx
+++ components/tables/DevelopmentCompaniesTable.tsx
@@ -11,13 +11,20 @@ interface DevelopmentCompany {
 
 interface DevelopmentCompaniesTableProps {
   projects: DevelopmentCompany[];
+  filterBy?: string;
 }
 
 const DevelopmentCompaniesTable: React.FC<DevelopmentCompaniesTableProps> = ({
   projects = [],
+  filterBy,
 }) => {
-  // Sort companies by GitHub presence
-  const sortedCompanies = projects.sort((a, b) => {
+  // Filter projects by tags if filterBy is provided
+  const filteredProjects = filterBy
+    ? projects.filter((project) => project.tags?.includes(filterBy))
+    : projects;
+
+  // Sort projects by GitHub presence
+  const sortedProjects = filteredProjects.sort((a, b) => {
     if (a.teamGithubURL && !b.teamGithubURL) return -1;
     if (!a.teamGithubURL && b.teamGithubURL) return 1;
     return 0;
@@ -27,7 +34,7 @@ const DevelopmentCompaniesTable: React.FC<DevelopmentCompaniesTableProps> = ({
     <div className="data-table-wrapper">
       <table className="data-table dev-companies-table">
         <tbody>
-          {sortedCompanies.map((company, index) => (
+          {sortedProjects.map((project, index) => (
             <tr
               key={index}
               className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20"
@@ -35,20 +42,20 @@ const DevelopmentCompaniesTable: React.FC<DevelopmentCompaniesTableProps> = ({
               <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell first-column-cell">
                 <div className="flex items-center justify-between w-full">
                   <span className="flex items-center">
-                    <Favicon url={company.website} />
+                    <Favicon url={project.website} />
                     &nbsp;
-                    <a href={company.website}>
+                    <a href={project.website}>
                       <span className="team_table_name_container">
-                        {company.name}
+                        {project.name}
                       </span>
                     </a>
                   </span>
                 </div>
               </td>
               <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 table-cell">
-                {company.teamGithubURL ? (
+                {project.teamGithubURL ? (
                   <TeamGithubBadge
-                    teamGithubURL={company.teamGithubURL}
+                    teamGithubURL={project.teamGithubURL}
                     text="Team"
                   />
                 ) : (
