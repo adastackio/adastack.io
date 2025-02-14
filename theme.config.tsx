08692699422dc266648824934af66d44c4554525
tuckpuck <tuckertriggs@pm.me>
2025-02-13 22:54:40 -0700

    improve SEO tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -30,26 +30,17 @@ const config: DocsThemeConfig = {
       : default_seo_description;
     return (
       <>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
         <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="canonical" href={url} />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <meta name="apple-mobile-web-app-title" content="adastack" />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta name="apple-mobile-web-app-title" content="adastack.io" />
       </>
     );
   },
